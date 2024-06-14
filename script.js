2a24eb5577ca6624282c6fa34770cc214752f8f3
Pranjali-Mohurle <pranjalimohurle67@gmail.com>
2024-06-14 20:08:02 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+let navbar=document.querySelector('.header .navbar');
+let searchForm=document.querySelector('.header .search-form');
+let loginForm=document.querySelector('.header .login-form');
+let contactInfo=document.querySelector('.contact-info');
+
+document.querySelector('#menu-btn').onclick=()=>{
+     navbar.classList.toggle('active');
+     searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+};
+
+document.querySelector('#search-btn').onclick=()=>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+};
+
+document.querySelector('#login-btn').onclick=()=>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+};
+
+document.querySelector('#info-btn').onclick=()=>{
+    contactInfo.classList.add('active');
+}
+
+document.querySelector('#close-contact-info').onclick=()=>{
+    contactInfo.classList.remove('active');
+}
+
+
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    contactInfo.classList.remove('active');
+};
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  
+  var swiper = new Swiper(".logo-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween:1.5,
+    breakpoints:{
+        0:{
+        slidesPreview: 2,
+        },
+        640:{
+            slidesPreview: 3,
+            },
+         768:{
+            slidesPreview: 4,
+            },
+          991:{
+            slidesPreview: 5,
+              },        
+    },
+    
+  });
+  
+
